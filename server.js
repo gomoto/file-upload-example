06c014c7d1036a4862002582234cf72e58d1e2ca
Ryan Gomoto <ryan@cyrusbio.com>
2019-05-07 23:50:44 -0700

    Catch multer errors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.post('/upload', multer().single('myfile'), (req, res) => {
+app.post('/upload', fileUploadMiddleware, (req, res) => {
   console.log('file', req.file);
   console.log(req.file.buffer.toString());
   console.log('body', req.body);
@@ -15,3 +15,17 @@ app.post('/upload', multer().single('myfile'), (req, res) => {
 app.listen(3000, () => {
   console.log('Server started');
 });
+
+function fileUploadMiddleware(req, res, next) {
+  multer().single('myfile')(req, res, (error) => {
+    if (error instanceof multer.MulterError) {
+      console.error('A Multer error occurred when uploading', error);
+      res.sendStatus(500);
+    } else if (error) {
+      console.error('An unknown error occurred when uploading', error);
+      res.sendStatus(500);
+    } else {
+      next();
+    }
+  });
+}
