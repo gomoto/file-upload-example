5356116d357592877b04448280c769d95d853f3d
Ryan Gomoto <ryan@cyrusbio.com>
2019-05-07 23:31:53 -0700

    Upload a file

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-const fileInput = document.getElementById('file-input');
+main()
 
-fileInput.onchange = function() {
-  const file = fileInput.files[0];
-  const formData = new FormData();
-  formData.append('file', file);
+function main() {
+  if (!doSupportUploadWithProgress()) {
+    return;
+  }
 
-  const xhr = new XMLHttpRequest();
-  // Add any event handlers here...
-  xhr.open('POST', 'http://localhost:3000/upload', true);
-  xhr.send(formData);
+  const fileInput = document.getElementById('file-input');
+
+  fileInput.onchange = function() {
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append('myfile', file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.upload.onloadstart = function(event) {
+      console.log('onloadstart', event);
+    };
+    xhr.upload.onprogress = function(event) {
+      console.log('onprogress', event);
+      if (event.lengthComputable) {
+        const percentComplete = event.loaded / event.total * 100;
+        console.log(`${percentComplete}%`);
+      } else {
+        console.log('Unable to compute progress information since the total size is unknown');
+      }
+    };
+    xhr.upload.onerror = function(event) {
+      console.log('onerror', event);
+    };
+    xhr.upload.onabort = function(event) {
+      console.log('onabort', event);
+    };
+    xhr.upload.onloadend = function(event) {
+      console.log('onloadend', event);
+    };
+    xhr.open('POST', 'http://localhost:3000/upload', true);
+    xhr.send(formData);
+  }
 }
 
+function doSupportUploadWithProgress() {
+  return doSupportFileAPI() && doSupportUploadProgressEvents() && doSupportFormData();
+
+  function doSupportFileAPI() {
+    const fi = document.createElement('INPUT');
+    fi.type = 'file';
+    return 'files' in fi;
+  };
+
+  function doSupportUploadProgressEvents() {
+    const xhr = new XMLHttpRequest();
+    return Boolean(xhr && ('upload' in xhr) && ('onprogress' in xhr.upload));
+  };
+
+  function doSupportFormData() {
+    return Boolean(window.FormData);
+  }
+}
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const multer = require('multer');
 
 const app = express();
 
 app.use(express.static(__dirname));
 
-app.post('/upload', (req, res) => {
-  console.log(req.body);
+app.post('/upload', multer().single('myfile'), (req, res) => {
+  console.log('file', req.file);
+  console.log(req.file.buffer.toString());
+  console.log('body', req.body);
   res.sendStatus(200);
 });
 
